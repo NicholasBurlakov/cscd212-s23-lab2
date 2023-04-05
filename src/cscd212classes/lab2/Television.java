6a2a890bdb34b8bb6592c5fc2425c17c233cdd96
Nick Burlakov <68251643+LordSirus@users.noreply.github.com>
2023-04-05 15:34:08 -0700

    Added Television methods

--- src/cscd212classes/lab2/Television.java
+++ src/cscd212classes/lab2/Television.java
@@ -1,6 +1,8 @@
 package cscd212classes.lab2;
 
-public class Television extends Object {
+import java.util.Objects;
+
+public class Television implements Comparable<Television>{
     private final boolean fourK;
     private final String make;
     private final String model;
@@ -22,5 +24,53 @@ public class Television extends Object {
         this.resolution = resolution;
         this.screenSize = screenSize;
         this.smart = smart;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getResolution() {
+        return resolution;
+    }
+
+    public int getScreenSize() {
+        return screenSize;
+    }
+
+
+    @Override
+    public int compareTo(Television o) {
+        return this.make.compareTo(o.make);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Television that = (Television) o;
+        return fourK == that.fourK && resolution == that.resolution && screenSize == that.screenSize && smart == that.smart && Objects.equals(make, that.make) && Objects.equals(model, that.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fourK, make, model, resolution, screenSize, smart);
+    }
+
+    //edit toString to fit the docs
+    @Override
+    public String toString() {
+        return "Television{" +
+                "fourK=" + fourK +
+                ", make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", resolution=" + resolution +
+                ", screenSize=" + screenSize +
+                ", smart=" + smart +
+                '}';
     }
 }
