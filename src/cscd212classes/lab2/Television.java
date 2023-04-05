6c062b4ca0152210b46039692679873eb1b6cc21
Nick Burlakov <68251643+LordSirus@users.noreply.github.com>
2023-04-05 15:18:18 -0700

    Television EVCs

--- src/cscd212classes/lab2/Television.java
+++ src/cscd212classes/lab2/Television.java
@@ -1,2 +1,26 @@
-package cscd212classes.lab2;public class Television {
+package cscd212classes.lab2;
+
+public class Television extends Object {
+    private final boolean fourK;
+    private final String make;
+    private final String model;
+    private final int resolution;
+    private final int screenSize;
+    private final boolean smart;
+
+    public Television(String make, String model, int resolution, int screenSize, boolean smart) {
+        this.make = make;
+        this.model = model;
+        this.resolution = resolution;
+        this.screenSize = screenSize;
+        this.smart = smart;
+    }
+
+    public Television(String make, String model, boolean smart, int screenSize, int resolution) {
+        this.make = make;
+        this.model = model;
+        this.resolution = resolution;
+        this.screenSize = screenSize;
+        this.smart = smart;
+    }
 }
